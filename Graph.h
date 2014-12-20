f7f595954fce0f0cf86600989927e3cc1f9a1d03
jseuribe <jseuribe@hotmail.com>
2014-12-20 12:08:45 -0500

    Update Graph.h

--- Graph.h
+++ Graph.h
@@ -1,20 +1,21 @@
 #ifndef GRAPH_H
-#include <utility>
 #include <vector>
 #include <map>
 #include <string>
 #include <iostream>
 #include "Vertex.h"
 
-//using namespace std;
+using namespace std;
 
 class Graph{
 private:
-    std::map<int, std::string> roots;
+    std::map<std::string, int> roots;
     std::vector<vertex> vertices;
 public:
     void add_vertex(std::string new_vertex);
     void add_adjacency(std::string first_name, std::string last_name, int weight);
+    void dfs(std::string name);
+    void print_adjacencies(std::string);
 };
 
 #endif
