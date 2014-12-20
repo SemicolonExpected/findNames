f59c5d170a787ef5c2ba154696dcdc62fa8b914d
jseuribe <jseuribe@hotmail.com>
2014-12-20 12:08:03 -0500

    Update Graph.cpp

--- Graph.cpp
+++ Graph.cpp
@@ -1,23 +1,24 @@
-#ifndef GRAPH_H
-#define GRAPH_H
 #include "Graph.h"
 #include "Vertex.h"
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
 
 void Graph::add_vertex(std::string new_vertex)
 {
     int pos = vertices.size();
-    roots[new_vertex] = vertices.size();
-
+    roots[new_vertex]  = pos;
     vertices[pos].name = new_vertex;
-
+    std::cout << "Graph pos " << roots[new_vertex] << " contains " << vertices[pos].name << std::endl;
 }
 
 void Graph::add_adjacency(std::string first_name, std::string last_name, int weight)
 {
     int pos1 = roots[first_name], pos2 = roots[last_name];
 
-    vertices[pos1].add_pair(weight, first_name);//Adjacency <first-last>
-    vertices[pos2].add_pair(weight, last_name);//Adjacency <last-first>
+    vertices[pos1].add_new_pair(weight, first_name);//Adjacency <first-last>
+    vertices[pos2].add_new_pair(weight, last_name);//Adjacency <last-first>
 
 }
 
@@ -25,18 +26,22 @@ void Graph::dfs(std::string name)
 {
     int pos = roots[name];
     pair<int, std::string> curr;
-    vertices[pos1].set_status(COMPLETE);
+    vertices[pos].set_status(COMPLETE);
 
     for(int i = 0; i < vertices.size(); i++)
     {
-        if(vertices[pos].vertex[i].visited() == COMPLETE) continue;
+        if(vertices[pos].status == COMPLETE) continue;
         else{
-            curr = vertices[pos].return_this_pair(std::string name);
+            curr = vertices[pos].return_this_pair(name);
             dfs(std::get<1>(curr));
         }
     }
 
 }
 
+void Graph::print_adjacencies(std::string name)
+{
+    int pos = roots[name];
 
-#endif
+    vertices[pos].print_out_adjacencies();
+}
