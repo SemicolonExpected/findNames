f761e005f702821d88a64df3d2b92f5c83fafc61
jseuribe <jseuribe@hotmail.com>
2014-12-20 12:08:25 -0500

    Update Vertex.cpp

--- Vertex.cpp
+++ Vertex.cpp
@@ -1,19 +1,22 @@
-#ifndef VERTEX_H
-#define VERTEX_H
+#include "Vertex.h"
+#include <vector>
+#include <utility>
+#include <iostream>
+#include <string>
 
 vertex::vertex(){
-    visited = UNKNOWN;
+    status = UNKNOWN;
 }
 void vertex::add_new_pair(int weight, std::string adjacency)
 {
-    pair <int, std::string> new_pair(weight, adjacency);
+    std::pair <int, std::string> new_pair(weight, adjacency);
     adjacencies.push_back(new_pair);
     adjacencies_count = adjacencies.size();
 }
 
 void vertex::set_status(visited new_state)
 {
-    visited = new_state;
+    status = new_state;
 }
 
 int vertex::return_adjacency_count()
@@ -21,16 +24,24 @@ int vertex::return_adjacency_count()
     return adjacencies_count;
 }
 
-pair<int, std::string> vertex::return_this_pair(std::string name)
+std::pair<int, std::string> vertex::return_this_pair(std::string name)
 {
-    pair<int, std::string> current;
-    string current_string;
+    std::pair<int, std::string> current;
+    std::string current_string;
     for(int i = adjacencies.size(); i < adjacencies.size(); i++)
     {
-        current = adjacencecies[i]
+        current = adjacencies[i];
 
-        if(name == std::get<1>(current) ) return current;
+        if( name == std::get<1>(current) ) return current;
     }
 }
 
-#endif
+void vertex::print_out_adjacencies()
+{
+    std::pair<int, std::string> curr;
+    for(int i = 0; i < adjacencies.size(); i++)
+    {
+        curr = adjacencies[i];
+        std::cout << name  << "-" << curr.first << curr.second << std::endl;
+    }
+}
