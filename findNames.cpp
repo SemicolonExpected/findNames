2d43d7839366cd34f52a077bd995c5f6ba346032
SemicolonExpected <SemicolonExpected@users.noreply.github.com>
2014-12-19 23:29:56 -0500

    Compiles

--- findNames.cpp
+++ findNames.cpp
@@ -3,12 +3,15 @@
  * 
  * *********************/
 
+#include "BinaryHeap.h"
+
+#include <vector>
 
 
 template <class T> unsigned int edit_distance(const T& s1, const T& s2)
 {
 	const size_t len1 = s1.size(), len2 = s2.size();
-	vector<vector<unsigned int> > d(len1 + 1, vector<unsigned int>(len2 + 1));
+	std::vector<std::vector<unsigned int> > d(len1 + 1, std::vector<unsigned int>(len2 + 1));
  
 	d[0][0] = 0;
 	for(unsigned int i = 1; i <= len1; ++i) d[i][0] = i;
@@ -21,7 +24,6 @@ template <class T> unsigned int edit_distance(const T& s1, const T& s2)
                                           d[i - 1][j - 1] + (s1[i - 1] == s2[j - 1] ? 0 : 1) );
 	return d[len1][len2];
 }
-//calculates minimum edit distance
 
 int main(){
 	
